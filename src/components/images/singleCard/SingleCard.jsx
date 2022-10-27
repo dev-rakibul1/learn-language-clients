afe0d0f5e6c06d16f7f2c3eb11b483be8f09ebd0
dev-rakibul1 <fl.rakibul@gmail.com>
2022-10-27 12:56:26 +0600

    React to print pdf done

--- src/components/images/singleCard/SingleCard.jsx
+++ src/components/images/singleCard/SingleCard.jsx
@@ -1,13 +1,33 @@
-import React from "react";
-import { FaRegEye, FaStar } from "react-icons/fa";
+import React, { useRef } from "react";
+import { toast } from "react-hot-toast";
+import { FaPrint, FaRegEye, FaStar } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
+import { useReactToPrint } from "react-to-print";
 
 const SingleCard = () => {
+  const pdfRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => pdfRef.current,
+    documentTitle: "technology-title",
+    onafterprint: () => toast.success("Print success"),
+  });
   const data = useLoaderData();
   const { images, id, description, price, title, rating } = data;
   console.log(data);
   return (
-    <div className="md:w-[75%] mx-auto px-2">
+    <div className="md:w-[75%] mx-auto px-2" ref={pdfRef}>
+      {/* print */}
+      <div
+        className="border inline-block me-auto ml-auto mb-4 text-end cursor-pointer"
+        onClick={handlePrint}
+        title="Print technology"
+      >
+        <small className="p-2  text-lg">Print</small>
+        <button className="text-end p-2">
+          <FaPrint className="text-end">Print</FaPrint>
+        </button>
+      </div>
+
       <div className="card w-full glass">
         <figure>
           <img src={images} alt="technology images!" className="w-[50%]" />
--- src/components/pages/single/SinglePart.jsx
+++ src/components/pages/single/SinglePart.jsx
@@ -1,13 +1,30 @@
-import React from "react";
-import { FaRegEye, FaStar } from "react-icons/fa";
+import React, { useRef } from "react";
+import { toast } from "react-hot-toast";
+import { FaPrint, FaRegEye, FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
-// 019 09 79 21 75
+import { useReactToPrint } from "react-to-print";
 
 const SinglePart = ({ data }) => {
+  const pdfRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => pdfRef.current,
+    documentTitle: "technology-title",
+    onafterprint: () => toast.success("Print success"),
+  });
   const { images, id, description, price, title, rating } = data;
   return (
-    <div className="w-[75%] mx-auto px-2">
+    <div className="w-[75%] mx-auto px-2" ref={pdfRef}>
+      {/* print */}
+      <div
+        className="border inline-block me-auto ml-auto mb-4 text-end cursor-pointer"
+        onClick={handlePrint}
+        title="Print technology"
+      >
+        <small className="p-2  text-lg">Print</small>
+        <button className="text-end p-2">
+          <FaPrint className="text-end">Print</FaPrint>
+        </button>
+      </div>
       <div className="card w-full glass">
         <figure>
           <img src={images} alt="technology images!" className="w-[50%]" />
